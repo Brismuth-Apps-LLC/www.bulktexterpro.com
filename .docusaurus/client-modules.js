ed35176c1778700f5255b0f4eecb561dd0a44814
Brian Smith <brismuth@gmail.com>
2022-11-01 21:21:04 -0600

    Adding docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Volumes/Code/www.bulktexterpro.com/node_modules/infima/dist/css/default/default.css'),
+  require('/Volumes/Code/www.bulktexterpro.com/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Volumes/Code/www.bulktexterpro.com/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Volumes/Code/www.bulktexterpro.com/src/css/custom.css'),
+];
